f6523c225c7f100a490fc90826ad273b7715d4a7
Bharath Subramanian <bharath.subramanian@cognizant.com>
2020-04-02 13:08:13 +0530

    added steps

--- src/main/java/com/cognizant/dashboardclient/plugins/models/TestStep.java
+++ src/main/java/com/cognizant/dashboardclient/plugins/models/TestStep.java
@@ -1,22 +1,15 @@
 package com.cognizant.dashboardclient.plugins.models;
 
-import lombok.Data;
-
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by 784420 on 3/9/2020 7:41 PM
- */
-@Data
-public class TestCase {
-    protected String name;
-    protected long duration;
+public class TestStep {
+    private String name;
+    private long duration;
     private String description;
     private Date startTime;
     private Date endTime;
-    private List<TestStep> steps;
-    protected String result;
+    private String result;
     private String uniqueId;
     private String parentId;
     private String remarks;
