557108ddbf0a647b2adf679b0d480224779165a0
Bharath Subramanian <bharath.subramanian@cognizant.com>
2020-04-22 17:02:39 +0530

    Added plural format

--- src/main/java/com/cognizant/dashboardclient/plugins/clients/TestReportClient.java
+++ src/main/java/com/cognizant/dashboardclient/plugins/clients/TestReportClient.java
@@ -9,13 +9,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.Map;
 
 public interface TestReportClient {
-    @PostMapping(value = "/test/report/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/test/reports", consumes = MediaType.APPLICATION_JSON_VALUE)
     ExecutiveTestPlan InsertOrUpdateTestPlan(@RequestBody ExecutiveTestPlan executiveTestPlan);
 
-    @PostMapping(value = "/test/report/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/test/reports", consumes = MediaType.APPLICATION_JSON_VALUE)
     ExecutiveTestPlan InsertOrUpdateTestPlan(@RequestBody ExecutiveTestPlan executiveTestPlan, @RequestHeader Map header);
 
-    @RequestMapping(value = "/attachments/"
+    @RequestMapping(value = "/attachments"
             , consumes = MediaType.MULTIPART_FORM_DATA_VALUE
             , produces = MediaType.APPLICATION_JSON_VALUE
             , method = RequestMethod.POST)
