08199835793c2cdaa7cf1c09cc5fd30dc82a4349
Ramanjaneyulu Kummari <ramanjaneyulu.kummari@cognizant.com>
2020-06-01 17:00:35 +0530

    renamed properties key names

--- src/main/java/com/cognizant/dashboardclient/plugins/common/BaseConstants.java
+++ src/main/java/com/cognizant/dashboardclient/plugins/common/BaseConstants.java
@@ -2,11 +2,11 @@ package com.cognizant.dashboardclient.plugins.common;
 
 public class BaseConstants {
     public static final String ENVIRONMENT = "environment";
-    public static final String PROJECT_NAME = "project.name";
-    public static final String EXECUTION_NAME = "execution.name";
-    public static final String TEST_REPORT_URL = "test.report.url";
-    public static final String LEAP_ENABLE = "leap.enable";
-    public static final String TEST_REPORT_TOKEN = "test.report.token";
+    public static final String PROJECT_NAME = "leap.report.project";
+    public static final String EXECUTION_NAME = "leap.report.execution";
+    public static final String TEST_REPORT_URL = "leap.report.host";
+    public static final String LEAP_ENABLE = "leap.report.enabled";
+    public static final String TEST_REPORT_TOKEN = "leap.report.token";
     public static final String LEAP_COLLECTOR_PROPERTIES = "leap-collector.properties";
 
     /*Log process message*/
